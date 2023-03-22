db93865e39ba1db0870d1d547bf449bd5d1e3ae7
ashwinlimbanee <ashwinlimbani23@gmail.com>
2023-03-22 07:14:03 +0530

    webcam

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { WebcamImage } from 'ngx-webcam';
+import { Observable, Subject } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'angular-test';
+export class AppComponent implements OnInit {
+  private trigger: Subject<any> = new Subject();
+  webcamImage!: WebcamImage;
+  private nextWebcam: Subject<any> = new Subject();
+  image = '';
+  showWebCam: boolean = false;
+
+  ngOnInit() {
+    // this.retrieveImage();
+  }
+
+  public get invokeObservable(): Observable<any> {
+    return this.trigger.asObservable();
+  }
+  public get nextWebcamObservable(): Observable<any> {
+    return this.nextWebcam.asObservable();
+  }
+
+  startCamera(): void {
+    this.showWebCam = true;
+  }
+
+  getSnapshot(): void {
+    this.trigger.next(void 0);
+  }
+
+  captureImg(webcamImage: WebcamImage): void {
+    this.webcamImage = webcamImage;
+    localStorage.setItem('image', webcamImage!.imageAsDataUrl);
+    this.retrieveImage();
+  }
+
+  retrieveImage() {
+    this.image = localStorage.getItem('image') as string;
+  }
 }
